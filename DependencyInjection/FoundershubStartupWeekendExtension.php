110640d0a0c2eca18ffc9b03724cf4ae6270a477
Markus Tacker <m@coderbyheart.de>
2015-05-17 18:41:54 +0200

    List sponsors.

--- DependencyInjection/FoundershubStartupWeekendExtension.php
+++ DependencyInjection/FoundershubStartupWeekendExtension.php
@@ -6,16 +6,33 @@ use Symfony\Component\Config\FileLocator;
 use Symfony\Component\DependencyInjection\ContainerBuilder;
 use Symfony\Component\DependencyInjection\Loader;
 use Symfony\Component\HttpKernel\DependencyInjection\Extension;
+use Symfony\Component\DependencyInjection\Extension\PrependExtensionInterface;
 
-class FoundershubStartupWeekendExtension extends Extension
+class FoundershubStartupWeekendExtension extends Extension implements PrependExtensionInterface
 {
     /**
      * {@inheritDoc}
      */
     public function load(array $configs, ContainerBuilder $container)
     {
-        $loader = new Loader\YamlFileLoader($container, new FileLocator(__DIR__.'/../Resources/config'));
+        $loader = new Loader\YamlFileLoader($container, new FileLocator(__DIR__ . '/../Resources/config'));
         $loader->load('services.yml');
         $loader->load('controllers.yml');
+        $loader->load('contentful.yml');
+    }
+
+    /**
+     * Allow an extension to prepend the extension configurations.
+     *
+     * @param ContainerBuilder $container
+     */
+    public function prepend(ContainerBuilder $container)
+    {
+        $contentfulConfig = array();
+
+        $contentfulConfig['thumbnails'] = array(
+            array('label' => 'sponsor', 'width' => 200, 'height' => 200, 'exact' => true),
+        );
+        $container->prependExtensionConfig('dothiv_contentful', $contentfulConfig);
     }
 }
